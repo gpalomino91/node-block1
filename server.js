2c3a4ccece9bae65297f6e4a15bfeab9f0a0d1d7
Gonzalo Palomino <104773712+gpalomino91@users.noreply.github.com>
2025-05-08 17:25:53 +0000

    Servidor express basico

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Servidor express funcionando');
+});
+
+app.listen(PORT, () => {
+    console.log(`Express escuchando en http://localhost:${PORT}`);
+})
